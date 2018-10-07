8d3068758d5331cf67ddcda0c66f9efd46fdde47
Artur Drzyzga <arturdrzyzga@MacBook-Pro-Artur.local>
2018-10-07 14:18:36 +0100

    additional info

---
+++ db/migrate/20181007125215_add_magic_columns_to_users.rb
@@ -0,0 +1,13 @@
+class AddMagicColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :login_count, :integer, :null => false, :default => 0
+    add_column :users, :failed_login_count, :integer, :null => false, :default => 0
+    add_column :users, :last_request_at, :datetime
+    add_column :users, :current_login_at, :datetime
+    add_column :users, :last_login_at, :datetime
+    add_column :users, :current_login_ip, :string
+    add_column :users, :last_login_ip, :string
+    add_column :users, :name, :string
+    add_column :users, :tel, :string
+  end
+end
