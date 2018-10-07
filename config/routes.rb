5ffa924696c7e7d08061e5ff9490094043890a5d
Artur Drzyzga <arturdrzyzga@MacBook-Pro-Artur.local>
2018-10-07 12:05:37 +0100

    dodanie authologic

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
+  resources :users, only: [:new, :create]
+  resources :user_sessions, only: [:create, :destroy]
   resources :tables
+  
+  delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
+  get '/sign_in', to: 'user_sessions#new', as: :sign_in
+
 
 
   root to: 'tables#index'
