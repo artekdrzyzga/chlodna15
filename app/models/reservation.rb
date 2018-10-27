10652a42cf19e8b6e4c657bfb6effca0b5ab338e
Dariusz Urbanczyk <dariusz.urbanczyk@calltracks.com>
2018-10-27 19:06:37 +0100

    add user_name

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,16 @@
 class Reservation < ApplicationRecord
     belongs_to :user
     belongs_to :table
+
+    def user_name
+      if user.present?
+        @user_name ||= user.name
+      else
+        @user_name = "Default"
+      end
+    end
+
+
+
+
 end
